7f113360d9eca6b22c2f150ac72184654d9c4c16
lukman-lua <65476464+lukman-lua@users.noreply.github.com>
2020-11-27 01:31:08 +0700

    add Auth Form for Login and Register

---
+++ PlaneAPP/Auth.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace PlaneAPP
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Auth));
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonDaftar = new System.Windows.Forms.Button();
+            this.cancelLogin = new System.Windows.Forms.Button();
+            this.usernameLogin = new System.Windows.Forms.TextBox();
+            this.passwordLogin = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            resources.ApplyResources(this.buttonLogin, "buttonLogin");
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonDaftar
+            // 
+            resources.ApplyResources(this.buttonDaftar, "buttonDaftar");
+            this.buttonDaftar.Name = "buttonDaftar";
+            this.buttonDaftar.UseVisualStyleBackColor = true;
+            // 
+            // cancelLogin
+            // 
+            resources.ApplyResources(this.cancelLogin, "cancelLogin");
+            this.cancelLogin.Name = "cancelLogin";
+            this.cancelLogin.UseVisualStyleBackColor = true;
+            // 
+            // usernameLogin
+            // 
+            resources.ApplyResources(this.usernameLogin, "usernameLogin");
+            this.usernameLogin.Name = "usernameLogin";
+            // 
+            // passwordLogin
+            // 
+            resources.ApplyResources(this.passwordLogin, "passwordLogin");
+            this.passwordLogin.Name = "passwordLogin";
+            // 
+            // usernameLabel
+            // 
+            resources.ApplyResources(this.usernameLabel, "usernameLabel");
+            this.usernameLabel.Name = "usernameLabel";
+            // 
+            // passwordLabel
+            // 
+            resources.ApplyResources(this.passwordLabel, "passwordLabel");
+            this.passwordLabel.Name = "passwordLabel";
+            // 
+            // Auth
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordLogin);
+            this.Controls.Add(this.usernameLogin);
+            this.Controls.Add(this.cancelLogin);
+            this.Controls.Add(this.buttonDaftar);
+            this.Controls.Add(this.buttonLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Auth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonDaftar;
+        private System.Windows.Forms.Button cancelLogin;
+        private System.Windows.Forms.TextBox usernameLogin;
+        private System.Windows.Forms.TextBox passwordLogin;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
---
+++ PlaneAPP/Auth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PlaneAPP
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;port=3306;uid=root;pwd=lukman122002;database=orderspeech;");
+            MySqlCommand command;
+            MySqlDataReader reader;
+            conn.Open();
+            String query = "SELECT * FROM admin WHERE user='" + usernameLogin.Text + "' AND password='" + passwordLogin.Text + "';";
+            command = new MySqlCommand(query, conn);
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                MessageBox.Show("login berhasil");
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password salah");
+            }
+            conn.Close();
+        }
+    }
+}
